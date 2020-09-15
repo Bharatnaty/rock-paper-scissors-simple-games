e0c9037439f4acd4b660ec49ccc7d079e326a648
Bharatnaty <47810389+Bharatnaty@users.noreply.github.com>
2020-09-15 10:34:58 +0530

    Add files via upload

---
+++ rock,paper,scissor.py
@@ -0,0 +1,25 @@
+from random import randint
+t = ["rock", "paper", "scissors"]
+computer = t[randint(0,2)]
+while True:
+    player = input("rock, paper, scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "scissors":
+        if computer == "rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("Check your spelling!")
+    break
